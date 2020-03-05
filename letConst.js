2853564c2235422720c586c64a7ee4bd53dd3c25
alaminse <contactalamin.se@gmail.com>
2020-03-06 01:56:47 +0600

    v-1.0

---
+++ letConst.js
@@ -0,0 +1,13 @@
+const name = "alamin";
+//constant value not changeable
+//name = "noman";
+console.log(name)
+const number = [12,23,4,5]
+number[1] = 22;
+number.push(10);
+console.log(number)
+
+for(let i=0;i<=10; i++){
+    console.log(i)
+}
+// console.log(i)
---
+++ template.js
@@ -0,0 +1,13 @@
+const firstName = "Md";
+const lastName = "Alamin";
+const fullName = firstName + " " + lastName;
+const fullName1 = `${firstName} ${lastName}`;
+console.log(fullName)
+console.log(fullName1)
+const test = "alamin\n"+
+    "noman";
+const test1 =`my name is
+alamin
+noman`;
+console.log(test)
+console.log(test1)
