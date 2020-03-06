c3247576637e01b0e9d50dc3383e5fa0d070604d
alaminse <contactalamin.se@gmail.com>
2020-03-06 16:36:32 +0600

    final-v

--- inheritance.js
+++ inheritance.js
@@ -1,1 +1,18 @@
-inheritance.js
+class Parents{
+    constructor(){
+        this.fatherName = "Alamin";
+    }
+    fullName(){
+        return this.name +" "+ this.fatherName;
+    }
+}
+class Child extends Parents{
+    constructor(name){
+        super();
+        this.name=name;
+    }
+}
+const baby = new Child("Khalid");
+const baby2 = new Child("Tom");
+console.log(baby.fullName())
+console.log(baby2)
